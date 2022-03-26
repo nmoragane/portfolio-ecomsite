6952d6a82389ae37620bdb1afe84fd0a2d0ffbd6
nmoragane <nmoragane@gmail.com>
2022-03-26 19:08:59 +0530

    setting up collections

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/Homepage';
+import ShopPage from './pages/shop/ShopPage';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+
       </Switch>
     </div>
   );
